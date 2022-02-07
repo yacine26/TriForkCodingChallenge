5b41f63dfbff8a331057497524b6ba64a2b95a97
yacine26 <mazouzyacine26@gmail.com>
2022-02-07 23:30:16 +0100

    added interface

---
+++ YacineCodingChallenge/Interfaces/IGroup.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YacineCodingChallenge.Interfaces
+{
+    interface IGroup
+    {
+        void AddMember(string MemberName);
+        void AddExpense(string MemberName, float Amount, string ExpenseName);
+        void ShowBalance(string MemberName);
+        void FinishTrip();
+    }
+}
